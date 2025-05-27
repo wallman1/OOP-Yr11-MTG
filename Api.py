4c135093c0b068fe2969abec2c30f34de0002a04
wallman1 <william.coote5@education.nsw.gov.au>
2025-05-28 09:23:13 +1000

    Worked on the bulk search system and saving to decks

--- Api.py
+++ Api.py
@@ -1,132 +1,51 @@
 import scrython
 import requests
 import time
-from requests import get
-from json import loads
+import json
+import os
 from shutil import copyfileobj
+from requests import get
 
-def Searchcard(getCard):
+DECK_FILE = "deck.json"
+
+def search_cards(query):
     try:
-        card = scrython.cards.Named(fuzzy=getCard)
-        if card.object() == 'error':
-            print(card.scryfallJson['details'])
+        response = scrython.cards.Search(q=query)
+        return response.data()
+    except Exception as e:
+        print("Search failed:", e)
+        return []
 
-        if "Creature" in card.type_line():
-            PT = "({}/{})".format(card.power(), card.toughness())
-        else:
-            PT = ""
+def get_image_from_url(url):
+    try:
+        response = get(url, stream=True)
+        if response.status_code == 200:
+            with open("image.png", "wb") as out_file:
+                copyfileobj(response.raw, out_file)
+            return True
+    except Exception as e:
+        print("Image download failed:", e)
+    return False
 
-        if card.cmc() == 0:
-            mana_cost = ""
-        else:
-            mana_cost = card.mana_cost()
+def save_to_deck(card_name):
+    deck = []
+    if os.path.exists(DECK_FILE):
+        with open(DECK_FILE, "r") as f:
+            try:
+                deck = json.load(f)
+            except json.JSONDecodeError:
+                deck = []
 
-        string = """
-        {cardname} {mana_cost}
-        {type_line} {set_code} {rarity}
-        {oracle_text}{power_toughness}
-        """.format(
-                cardname=card.name(),
-                mana_cost=mana_cost,
-                type_line=card.type_line(),
-                set_code=card.set_code().upper(),
-                rarity=card.rarity(),
-                oracle_text=card.oracle_text(),
-                power_toughness=PT
-                )
+    deck.append(card_name)
 
-        return(string)
+    with open(DECK_FILE, "w") as f:
+        json.dump(deck, f)
 
-    except:
-        print("404 Card Not Found")
-
-    IMAGE_PATH = "Documents\GitHub\OOP-Yr11-MTG" # You can replace this with whatever path
-
-    def save_image(path, url, name):
-        response = requests.get(url)
-
-        with open('{}{}.png'.format(path, name), 'wb') as f:
-            f.write(response.content)
-
-    def get_all_pages(set_code):
-        page_count = 1
-        all_data = []
-        while True:
-            time.sleep(0.5)
-            page = scrython.cards.Search(q='e:{}'.format(set_code), page=page_count)
-            all_data = all_data + page.data()
-            page_count += 1
-            if not page.has_more():
-                break
-
-        return all_data
-
-    def get_all_cards(card_array):
-        card_list = []
-        for card in card_array:
-            time.sleep(0.5)
-            id_ = card['id']
-            card = scrython.cards.Id(id=id_)
-            card_list.append(card)
-        return card_list
-
-def get_image(name):
-    card = scrython.cards.Named(fuzzy=name)
-    cardname=card.name()
-    card = loads(get(f"https://api.scryfall.com/cards/search?q={cardname}").text)
- 
-    # Get the image URL
-    img_url = card['data'][0]['image_uris']['png']
-        
-    # Save the image
-    with open('image.png', 'wb') as out_file:
-        copyfileobj(get(img_url, stream = True).raw, out_file)
-
-class creature():
-    def __init__(self):
-        pass
-
-    def haste():
-        pass
-
-    def detain():
-        pass
-
-    def evolve():
-        pass
-
-    def hexproof():
-        pass
-
-    def amass():
-        pass
-
-    def flying():
-        pass
-
-    def reach():
-        pass
-
-    def deathtouch():
-        pass
-
-    def doublestrike():
-        pass
-
-    def firststrike():
-        pass
-
-    def trample():
-        pass
-
-    def defender():
-        pass
-
-    def lifelink():
-        pass
-
-    def menace():
-        pass
-
-    def vigilance():
-        pass
+def load_deck():
+    if os.path.exists(DECK_FILE):
+        with open(DECK_FILE, "r") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+    return []
--- GUI.py
+++ GUI.py
@@ -1,95 +1,211 @@
-# Python program to transform the 
-# image with the mouse 
+import pygame
+import sys
+from Api import search_cards, get_image_from_url, save_to_deck, load_deck
 
-#Import the libraries pygame and math 
-import pygame as pg
-import math 
-from pygame.locals import *
-import Api
+# --- Pygame setup ---
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+WIDTH, HEIGHT = screen.get_size()
+pygame.display.set_caption("MTG Deck Builder")
+font = pygame.font.SysFont(None, 32)
+large_font = pygame.font.SysFont(None, 48)
 
-# Take colors input 
-pg.init()
-screen = pg.display.set_mode((640, 480))
-COLOR_INACTIVE = pg.Color('lightskyblue3')
-COLOR_ACTIVE = pg.Color('dodgerblue2')
-FONT = pg.font.Font(None, 32)
-txt = ""
-img = pg.image.load("C:\Users\willi\OneDrive\Documents\stuffmcstuff\OOP-YR11-MTG\image.png").convert()
-screen.blit(img, (0, 0))
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (70, 70, 70)
+BLUE = pygame.Color('dodgerblue2')
+DARK = (25, 25, 25)
 
-class InputBox:
+# States
+HOME, SEARCH, DECKS, DECK_VIEW = "HOME", "SEARCH", "DECKS", "DECK_VIEW"
+state = HOME
 
-    def __init__(self, x, y, w, h, text=''):
-        self.rect = pg.Rect(x, y, w, h)
-        self.color = COLOR_INACTIVE
-        self.text = text
-        self.txt_surface = FONT.render(text, True, self.color)
-        self.active = False
+# Input box setup
+input_box = pygame.Rect(100, 100, 600, 40)
+deck_input_box = pygame.Rect(100, 160, 600, 40)
+color_inactive = pygame.Color('lightskyblue3')
+color_active = BLUE
+color = color_inactive
+active = False
+deck_active = False
+text = ''
+deck_name = ''
+search_results = []
+selected_card = None
+card_image = None
+deck_cards = []
+current_deck = ''
 
-    def handle_event(self, event):
-        if event.type == pg.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box rect.
-            if self.rect.collidepoint(event.pos):
-                # Toggle the active variable.
-                self.active = not self.active
-            else:
-                self.active = False
-            # Change the current color of the input box.
-            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
-        if event.type == pg.KEYDOWN:
-            if self.active:
-                if event.key == pg.K_RETURN:
-                    print(self.text)
-                    global txt
-                    txt = Api.Searchcard(self.text)
-                    self.text = ''
-                elif event.key == pg.K_BACKSPACE:
-                    self.text = self.text[:-1]
+# Buttons
+search_button = pygame.Rect(WIDTH // 2 - 100, 300, 200, 60)
+decks_button = pygame.Rect(WIDTH // 2 - 100, 400, 200, 60)
+back_button = pygame.Rect(20, 20, 100, 40)
+save_button = pygame.Rect(WIDTH // 2 - 100, HEIGHT - 100, 200, 50)
+load_deck_buttons = []
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    screen.fill(DARK)
+    mouse_pos = pygame.mouse.get_pos()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if state == HOME:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if search_button.collidepoint(event.pos):
+                    state = SEARCH
+                elif decks_button.collidepoint(event.pos):
+                    deck_cards = list(load_deck())
+                    load_deck_buttons = [pygame.Rect(100, 140 + i * 50, 600, 40) for i in range(len(deck_cards))]
+                    state = DECKS
+
+        elif state == SEARCH:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = True
+                    deck_active = False
+                elif deck_input_box.collidepoint(event.pos):
+                    deck_active = True
+                    active = False
                 else:
-                    self.text += event.unicode
-                # Re-render the text.
-                self.txt_surface = FONT.render(self.text, True, self.color)
+                    active = False
+                    deck_active = False
 
-    def update(self):
-        # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width()+10)
-        self.rect.w = width
+                color = color_active if active else color_inactive
 
-    def draw(self, screen):
-        # Blit the text.
-        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
-        # Blit the rect.
-        text1 = FONT.render(txt, True, COLOR_INACTIVE)
-        textRect1 = text1.get_rect()
-        textRect1.center = (250, 250)
-        pg.display.flip()
-        screen.blit(text1, textRect1)
-        pg.draw.rect(screen, self.color, self.rect, 2)
+                if back_button.collidepoint(event.pos):
+                    state = HOME
+                    search_results = []
+                    selected_card = None
+                    card_image = None
 
-def main():
-    clock = pg.time.Clock()
-    input_box1 = InputBox(100, 100, 140, 32)
-    input_boxes = [input_box1]
-    done = False
+                if 220 < event.pos[1] < 520:
+                    index = (event.pos[1] - 220) // 60
+                    if index < len(search_results):
+                        selected_card = search_results[index]
+                        if 'image_uris' in selected_card:
+                            img_url = selected_card['image_uris']['png']
+                            if get_image_from_url(img_url):
+                                card_image = pygame.image.load("image.png")
 
-    while not done:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-            for box in input_boxes:
-                box.handle_event(event)
+                if save_button.collidepoint(event.pos) and selected_card and deck_name:
+                    save_to_deck(deck_name, selected_card['name'])
 
-        for box in input_boxes:
-            box.update()
+            elif event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        search_results = search_cards(text)
+                        selected_card = None
+                        card_image = None
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
 
-        screen.fill((30, 30, 30))
-        for box in input_boxes:
-            box.draw(screen)
+                elif deck_active:
+                    if event.key == pygame.K_BACKSPACE:
+                        deck_name = deck_name[:-1]
+                    else:
+                        deck_name += event.unicode
 
-        pg.display.flip()
-        clock.tick(30)
+        elif state == DECKS:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back_button.collidepoint(event.pos):
+                    state = HOME
+                for i, btn in enumerate(load_deck_buttons):
+                    if btn.collidepoint(event.pos):
+                        current_deck = deck_cards[i]
+                        deck_cards = load_deck().get(current_deck, [])
+                        state = DECK_VIEW
 
+        elif state == DECK_VIEW:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if back_button.collidepoint(event.pos):
+                    state = DECKS
 
-if __name__ == '__main__':
-    main()
-    pg.quit()
+    # --- Drawing ---
+    if state == HOME:
+        title = large_font.render("Magic: The Gathering Deck Builder", True, WHITE)
+        screen.blit(title, (WIDTH // 2 - title.get_width() // 2, 100))
+
+        pygame.draw.rect(screen, BLUE, search_button)
+        label = font.render("Start Searching", True, WHITE)
+        screen.blit(label, (search_button.x + 30, search_button.y + 15))
+
+        pygame.draw.rect(screen, BLUE, decks_button)
+        decks_label = font.render("View Decks", True, WHITE)
+        screen.blit(decks_label, (decks_button.x + 50, decks_button.y + 15))
+
+    elif state == SEARCH:
+        pygame.draw.rect(screen, GREY, back_button)
+        back_text = font.render("Back", True, WHITE)
+        screen.blit(back_text, (back_button.x + 10, back_button.y + 10))
+
+        txt_surface = font.render(text, True, color)
+        input_box.w = max(400, txt_surface.get_width() + 10)
+        screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        pygame.draw.rect(screen, color, input_box, 2)
+
+        deck_surface = font.render(deck_name, True, color_active if deck_active else color_inactive)
+        screen.blit(deck_surface, (deck_input_box.x + 5, deck_input_box.y + 5))
+        pygame.draw.rect(screen, color_active if deck_active else color_inactive, deck_input_box, 2)
+
+        for i, card in enumerate(search_results[:5]):
+            y = 220 + i * 60
+            pygame.draw.rect(screen, GREY, (100, y, 600, 50))
+            name_surf = font.render(card['name'], True, WHITE)
+            screen.blit(name_surf, (110, y + 10))
+
+        if selected_card and card_image:
+            scaled_img = pygame.transform.scale(card_image, (488, 680))
+            screen.blit(scaled_img, (WIDTH // 2 - 244, HEIGHT - 740))
+
+            pygame.draw.rect(screen, BLUE, save_button)
+            save_text = font.render("Save to Deck", True, WHITE)
+            screen.blit(save_text, (save_button.x + 30, save_button.y + 15))
+
+    elif state == DECKS:
+        screen.blit(large_font.render("Your Decks", True, WHITE), (WIDTH // 2 - 100, 60))
+        pygame.draw.rect(screen, GREY, back_button)
+        back_text = font.render("Back", True, WHITE)
+        screen.blit(back_text, (back_button.x + 10, back_button.y + 10))
+
+        for i, name in enumerate(deck_cards):
+            pygame.draw.rect(screen, GREY, load_deck_buttons[i])
+            name_surf = font.render(name, True, WHITE)
+            screen.blit(name_surf, (load_deck_buttons[i].x + 10, load_deck_buttons[i].y + 10))
+
+    elif state == DECK_VIEW:
+        screen.blit(large_font.render(f"Deck: {current_deck}", True, WHITE), (WIDTH // 2 - 100, 60))
+        pygame.draw.rect(screen, GREY, back_button)
+        back_text = font.render("Back", True, WHITE)
+        screen.blit(back_text, (back_button.x + 10, back_button.y + 10))
+
+        for i, name in enumerate(deck_cards):
+            y = 120 + i * 40
+            name_surf = font.render(name, True, WHITE)
+            screen.blit(name_surf, (100, y))
+
+            # Hover detection and card image
+            if 100 <= mouse_pos[0] <= 700 and y <= mouse_pos[1] <= y + 40:
+                try:
+                    temp = search_cards(name)
+                    if temp and 'image_uris' in temp[0]:
+                        if get_image_from_url(temp[0]['image_uris']['png']):
+                            hover_img = pygame.image.load("image.png")
+                            scaled_hover = pygame.transform.scale(hover_img, (244, 340))
+                            screen.blit(scaled_hover, (WIDTH - 260, y))
+                except:
+                    pass
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
